50a9b31d034e7b28971ee348512073d4673f4fa2
riteshfilmscreation <riteshfilmscreation@gmail.com>
2025-07-08 01:29:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
-const { MongoClient, ServerApiVersion } = require('mongoose');
+const { MongoClient } = require('mongodb');
 const WebSocket = require('ws');
 const qrcode = require('qrcode-terminal');
 require('dotenv').config();
 
 const mongoURI = "mongodb+srv://riteshfilmscreation:riteshfilmscreation@riteshfilmscreation.lspmkds.mongodb.net/?retryWrites=true&w=majority&appName=riteshfilmscreation";
 // const mongoURI = process.env.MONGO_URI;
+console.log("testing");
 const mongoClient = new MongoClient(mongoURI, {
   serverApi: {
     version: ServerApiVersion.v1,
